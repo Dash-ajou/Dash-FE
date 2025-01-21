51523b91cd53d04195863c379dbfb47156cd6b94
aneomagig <imapversion4@gmail.com>
2025-01-22 02:02:23 +0900

    feat: ListBlock 컴포넌트 생성

---
+++ src/components/common/ListBlock.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type ListButtonProps = {
+    coupnum: string;
+    coupstatus?: "Issued" | "Registered" | "Used";
+    name?: string;
+};
+
+const ListButton: React.FC<ListButtonProps> = ({ coupnum, coupstatus, name }) => {
+    // 상태에 따른 텍스트 생성
+    const getContent = () => {
+        switch (coupstatus) {
+            case "Issued":
+                return "미등록";
+            case "Registered":
+                return `${name ? `${name}/` : ""}등록완료`;
+            case "Used":
+                return `${name ? `${name}/` : ""}사용완료`;
+            default:
+                return ""; // 상태가 없을 경우 기본값
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-between w-full px-6 py-4 mx-6 rounded-[12px] bg-main05 text-[12px] text-black font-normal my-1">
+            {/* 왼쪽: coupnum */}
+            <span>{coupnum}</span>
+            {/* 오른쪽: 상태 및 이름 */}
+            <span>{getContent()}</span>
+        </div>
+    );
+};
+
+export default ListButton;
