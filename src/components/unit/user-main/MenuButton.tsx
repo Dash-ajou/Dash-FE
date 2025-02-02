ff3e661af6d31ff9d4b659a6ae70e96cb0ea1776
aneomagig <imapversion4@gmail.com>
2025-02-02 18:27:45 +0900

    feat: MenuButton 컴포넌트 제작

--- src/components/unit/user-main/MenuButton.tsx
+++ src/components/unit/user-main/MenuButton.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const MenuButton: React.FC = () => {
+type MenuButtonType = "newcoup" | "gift" | "couprequest" | "couplist";
+
+interface MenuButtonProps {
+	type: MenuButtonType;
+}
+
+const MenuButton: React.FC<MenuButtonProps> = ({ type }) => {
+    const navigate = useNavigate();
+
+    const getText = (type: MenuButtonType): string => {
+        switch (type) {
+            case "newcoup":
+                return "쿠폰 등록";
+            case 'gift':
+                return "선물함";
+            case 'couprequest':
+                return "쿠폰 발행 요청";
+            case 'couplist':
+                return "쿠폰 배부 내역";
+            default: 
+                return "";
+        }
+    };
+
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center">
+            <button
+            className={`w-16 h-16 rounded-lg shadow-custom flex flex-col items-center justify-center bg-white menu-button-#{type}`}
+            onClick={() => navigate('/')} /* 일단 비워둠 */
+            >
+            <div className="w-10 h-10 flex items-center justify-center mb-1">
+                {/* 아이콘 */}
+            </div>
+        </button>
+        <span className="mt-1 text-xs font-medium text-black">{getText(type)}</span>
         </div>
+
     )
 };
 
