4edfe23daa2ab089e451456e7c416b5fb7fe1402
aneomagig <imapversion4@gmail.com>
2025-02-02 15:42:19 +0900

    feat: block component 제작

--- src/components/module/Block.tsx
+++ src/components/module/Block.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const Block: React.FC = () => {
-    return (
-        <div>
-        </div>
-    )
+type BlockType = "button" | "toggle" | "detail";
+
+interface BlockProps {
+	type: BlockType;
+	title: string;
+	subtitle?: string;
+	info?: string;
+	action?: React.ReactNode;
+}
+
+const Block: React.FC<BlockProps> = ({
+	type,
+	title,
+	subtitle,
+	info,
+	action,
+}) => {
+	return (
+		<div className="flex justify-between items-center bg-white shadow-md rounded-lg p-4 mb-4">
+			<div>
+				<div className="font-bold text-lg">{title}</div>
+				{subtitle && <div className="text-sm text-black">{subtitle}</div>}
+				{info && <div className="text-sm text-black">{info}</div>}
+			</div>
+
+			{/* 오른쪽 영역*/}
+			<div>{action}</div>
+		</div>
+	);
 };
 
 export default Block;
