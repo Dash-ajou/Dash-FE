6629484ff93d3a7aed6441e376674e26d7cbd547
aneomagig <imapversion4@gmail.com>
2025-02-04 16:37:21 +0900

    style: 코드 리뷰 내용 수정

--- src/components/common/DetailBox.tsx
+++ src/components/common/DetailBox.tsx
@@ -8,7 +8,7 @@ type DetailBoxProps = {
   rightstring?: string[];
   rightButtonLinks?: string[];
   statusType?: "used" | "unused";
-  statusColor?: "blue" | "red" | "gray" | "green" | "button";
+  statusColor?: "red" | "green";
 };
 
 const DetailBox: React.FC<DetailBoxProps> = ({
@@ -21,47 +21,40 @@ const DetailBox: React.FC<DetailBoxProps> = ({
   statusColor = "gray",
 }) => {
   const leftStringStyle =
-    mode === "setting" ? "font-normal text-[16px] text-black" : "font-semibold text-sm";
-  
-  return (
-    <div
-      className={`border rounded-xl px-4 py-5 shadow-custom-basic bg-white w-full`}
-      style={{
-        height: "auto",
-      }}
-    >
-      <div className="flex justify-between mb-3">
-      <h3 className="px-1 text-base font-semibold text-blue-500">{title}</h3>
-      {mode === "coupinfo" && statusType && (
-        <Status 
-          statusType={statusType}
-          color={statusColor}
-        />
-      )}
+    mode === "setting" ? "font-normal text-base text-black" : "font-semibold text-sm";
+    return (
+      <div
+        className={`border rounded-xl px-4 py-5 shadow-custom-basic bg-white w-full`}
+        style={{
+          height: "auto",
+        }}
+      >
+        <div className="flex justify-between mb-3">
+        <h3 className="px-1 text-base font-semibold text-blue-500">{title}</h3>
+        {mode === "coupinfo" && statusType && (
+          <Status 
+            statusType={statusType}
+            color={statusColor as "red" | "green"}
+          />
+        )}
+        </div>
+        <hr className="w-full h-[1px] bg-gray-500 mx-auto mb-4 "/>
+        <div className="px-1 space-y-3">
+          {leftstring.map((left, index) => (
+            <div
+              key={index}
+              className="flex items-start gap-3"
+            >
+              <span className={`${leftStringStyle} text-black min-w-[90px]`}>
+                {left}
+              </span>
+              <span className="text-black text-sm font-light text-left flex-1">
+                {rightstring[index] || ""}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-      
-      <div className=" w-full h-[1px] bg-gray-500 mx-auto mb-4"></div>
-      
-      <div className="px-1 space-y-3">
-        {leftstring.map((left, index) => (
-          <div
-            key={index}
-            className="flex items-start"
-            style={{
-              gap: "12px",
-            }}
-          >
-            <span className={`${leftStringStyle} text-black min-w-[90px]`}>
-              {left}
-            </span>
-            <span className="text-black text-sm font-light text-left flex-1">
-              {rightstring[index] || ""}
-            </span>
-            
-          </div>
-        ))}
-      </div>
-    </div>
   );
 };
 
