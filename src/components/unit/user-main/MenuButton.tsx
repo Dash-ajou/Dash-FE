688a7c1031765ecc8aa729157efaae3f2a78a80f
aneomagig <imapversion4@gmail.com>
2025-02-04 20:30:59 +0900

    feat: Icon 컴포넌트와 연동

--- src/components/unit/user-main/MenuButton.tsx
+++ src/components/unit/user-main/MenuButton.tsx
@@ -1,46 +1,45 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Icon from "../../common/icons/Icon";
- 
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type MenuButtonType = "newcoup" | "gift" | "couprequest" | "couplist";
 
 interface MenuButtonProps {
-  type: "newcoup" | "gift" | "couprequest" | "couplist";
+	type: MenuButtonType;
 }
 
 const MenuButton: React.FC<MenuButtonProps> = ({ type }) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const getContent = () => {
-    switch (type) {
-        case "newcoup":
-            return { icon: <Icon name="newcoupicon" size={30}/>, message: "쿠폰 등록" };
-        case "gift":
-            return { icon: <Icon name="gifticon" size={30}/>, message: "선물함" };
-        case "couprequest":
-            return { icon: <Icon name="couprequesticon" size={30}/>, message: "쿠폰 발행 요청" };
-        case "couplist":
-            return { icon: <Icon name="couplisticon" size={30}/>, message: "쿠폰 배부 내역" };
-        default:
-            return { icon: <></>, message: ""};
-    }
-  }
-  const { icon, message } = getContent();
+    const getText = (type: MenuButtonType): string => {
+        switch (type) {
+            case "newcoup":
+                return "쿠폰 등록";
+            case 'gift':
+                return "선물함";
+            case 'couprequest':
+                return "쿠폰 발행 요청";
+            case 'couplist':
+                return "쿠폰 배부 내역";
+            default: 
+                return "";
+        }
+    };
 
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <button
-        className={`flex rounded-lg shadow-custom-basic flex-col items-center justify-center bg-white menu-button-${type}`}
-        onClick={() => navigate("/")}
-      >
-        <div className="flex items-center justify-center p-2">
-          {icon} {/* 아이콘 렌더링 */}
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <button
+            className={`w-16 h-16 rounded-lg shadow-custom flex flex-col items-center justify-center bg-white menu-button-#{type}`}
+            onClick={() => navigate('/')} /* 일단 비워둠 */
+            >
+            <div className="w-10 h-10 flex items-center justify-center mb-1">
+                {/* 아이콘 */}
+            </div>
+        </button>
+        <span className="mt-1 text-xs font-medium text-black">{getText(type)}</span>
         </div>
-      </button>
-      <span className="mt-2 text-xs font-medium text-black">
-        {message}
-      </span>
-    </div>
-  );
+
+    )
 };
+
 
 export default MenuButton;
