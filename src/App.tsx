35684af425d9f6d664c3b6e4c4c4a7d06eb6b8ba
aneomagig <imapversion4@gmail.com>
2025-02-04 21:00:16 +0900

    style: 테스트용 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import React from 'react';
-import MenuButton from './components/unit/user-main/MenuButton'; // MenuButton 컴포넌트 경로에 맞게 수정 필요
+import React from "react";
 
-const App: React.FC = () => {
-  return (
-    <div className="flex flex-wrap gap-4 p-4 bg-gray-100 min-h-screen">
-      {/* 각 type에 맞는 MenuButton 렌더링 */}
-      <MenuButton type="newcoup" />
-      <MenuButton type="gift" />
-      <MenuButton type="couprequest" />
-      <MenuButton type="couplist" />
-    </div>
-  );
-};
+
 
 export default App;
