df1566895a50a5d2c7c546b625d25761c6616b7d
aneomagig <imapversion4@gmail.com>
2025-02-17 15:06:31 +0900

    feat: CouponCount 컴포넌트 제작

--- src/components/unit/user-mypage/CouponCount.tsx
+++ src/components/unit/user-mypage/CouponCount.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Icon from '../../common/icons/Icon';
 
-const CouponCount: React.FC = () => {
+type CouponCountProps = {
+    numofcoup: number;
+}
+
+const CouponCount: React.FC<CouponCountProps> = ( {numofcoup} ) => {
     return (
-        <div>
+        <div className='flex flex-col bg-white rounded-xl shadow-custom-basic'>
+            <div className='px-4 py-3'>
+                <h1 className='text-sm text-black font-semibold pb-2'>사용 가능한 쿠폰</h1>
+                <div className='flex gap-4'>
+                <Icon name="couponicon_line" size={40}/>
+                <p className='text-2xl font-black text-black '>{numofcoup}</p>
+            </div>
+            </div>
+            
         </div>
     )
 };
