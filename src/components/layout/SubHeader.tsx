d2214b5e5fd8813b85f5a2535d39837a81fbfd62
aneomagig <imapversion4@gmail.com>
2025-02-18 17:02:20 +0900

    feat: SubHeader 생성

---
+++ src/components/layout/SubHeader.tsx
@@ -0,0 +1,20 @@
+import Icon from "../common/icons/Icon";
+import { useLocation, useNavigate } from "react-router-dom";
+import pageTitles from "../../constants/pageTitles";
+
+const SubHeader = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    
+    const pageTitle = pageTitles[location.pathname] || "페이지 없음"; // 없는 경우 기본값 설정
+  
+    return (
+      <header className="w-full flex items-center p-5 bg-white">
+        <Icon name="arrowicon_line_right" onClick={() => navigate(-1)} />
+        <h1 className="ml-4 text-2xl font-bold">{pageTitle}</h1>
+      </header>
+    );
+  };
+  
+  export default SubHeader;
+  
