e77667928857c298bad071e7fee34a84e95c830f
aneomagig <imapversion4@gmail.com>
2025-03-17 15:02:41 +0900

    style: DetailBox 코드 디자인 수정

--- src/components/common/DetailBox.tsx
+++ src/components/common/DetailBox.tsx
@@ -21,40 +21,39 @@ const DetailBox: React.FC<DetailBoxProps> = ({
   statusColor = "gray",
 }) => {
   const leftStringStyle =
-    mode === "setting" ? "font-normal text-base text-black" : "font-semibold text-sm";
-    return (
-      <div
-        className={`border rounded-xl px-4 py-5 shadow-custom-basic bg-white w-full`}
-        style={{
-          height: "auto",
-        }}
-      >
-        <div className="flex justify-between mb-3">
+    mode === "setting"
+      ? "font-normal text-base text-black"
+      : "font-semibold text-sm";
+  return (
+    <div
+      className={`border rounded-xl px-4 py-5 shadow-custom-basic bg-white w-full`}
+      style={{
+        height: "auto",
+      }}
+    >
+      <div className="flex justify-between mb-3">
         <h3 className="px-1 text-base font-semibold text-blue-500">{title}</h3>
         {mode === "coupinfo" && statusType && (
-          <Status 
+          <Status
             statusType={statusType}
             color={statusColor as "red" | "green"}
           />
         )}
-        </div>
-        <hr className="w-full h-[1px] bg-gray-500 mx-auto mb-4 "/>
-        <div className="px-1 space-y-3">
-          {leftstring.map((left, index) => (
-            <div
-              key={index}
-              className="flex items-start gap-3"
-            >
-              <span className={`${leftStringStyle} text-black min-w-[90px]`}>
-                {left}
-              </span>
-              <span className="text-black text-sm font-light text-left flex-1">
-                {rightstring[index] || ""}
-              </span>
-            </div>
-          ))}
-        </div>
       </div>
+      <hr className="w-full border-t border-black h-[1px] mx-auto mb-4" />
+      <div className="px-1 space-y-3">
+        {leftstring.map((left, index) => (
+          <div key={index} className="flex items-start gap-3">
+            <span className={`${leftStringStyle} text-black min-w-[90px]`}>
+              {left}
+            </span>
+            <span className="text-black text-sm font-light text-left flex-1">
+              {rightstring[index] || ""}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
