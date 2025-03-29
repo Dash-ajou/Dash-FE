b7b90579bb161b29c852d4c8f42f282f3cb0aaa5
aneomagig <imapversion4@gmail.com>
2025-03-29 23:56:20 +0900

    feat: Routes에 Layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React, {useState} from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserMain from "./pages/UserMain";
+import UserNotification from "./pages/UserNotification";
+import UserMypage from "./pages/UserMypage";
+import UserGift from "./pages/UserGift";
+import Layout from "./components/layout/Layout";
 
 const App: React.FC = () => {
     return (
-      <div>
-
-      </div>
+        <Routes>
+            <Route
+                path="/usermain"
+                element={
+                    <Layout>
+                        <UserMain />
+                    </Layout>
+                }
+            />
+            <Route
+                path="/usernotification"
+                element={
+                    <Layout>
+                        <UserNotification />
+                    </Layout>
+                }
+            />
+            <Route
+                path="/usermypage"
+                element={
+                    <Layout>
+                        <UserMypage />
+                    </Layout>
+                }
+            />
+            <Route
+                path="/usergift"
+                element={
+                    <Layout>
+                        <UserGift />
+                    </Layout>
+                }
+            />
+        </Routes>
     );
-  };
-  
-  export default App;
+};
+
+export default App;
