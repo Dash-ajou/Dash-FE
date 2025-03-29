4a6f13a206390d3d4eb6ccc1534a68ec4362e591
aneomagig <imapversion4@gmail.com>
2025-03-30 00:20:52 +0900

    fix: Header 삭제, 불필요한 div 삭제

--- src/pages/UserMypage.tsx
+++ src/pages/UserMypage.tsx
@@ -1,57 +1,51 @@
 import CommonButton from "../components/common/button/CommonButton";
 import DetailBox from "../components/common/DetailBox";
-import MainHeader from "../components/layout/MainHeader";
 import CouponCount from "../components/unit/user-mypage/CouponCount";
 
 const UserMypage = () => {
-  return (
-    <div>
-      <div>
-        <MainHeader />
-      </div>
-      <div className="mx-7">
-        <div className="text-black font-bold text-2xl mt-11 mb-4">
-          <p>케인님 안녕하세요</p>
+    return (
+        <div className="mx-7">
+            <div className="text-black font-bold text-2xl mt-11 mb-4">
+                <p>ㅇㅇ님 안녕하세요</p>
+            </div>
+            <div className="flex justify-center gap-3">
+                <div className="w-full">
+                    <CouponCount type="available" />
+                </div>
+                <div className="w-full">
+                    <CouponCount type="used" />
+                </div>
+            </div>
+            <div className="flex flex-col mt-7 gap-4">
+                <DetailBox
+                    mode="setting"
+                    title="내 정보"
+                    leftstring={["계정 정보", "비밀번호 변경하기"]}
+                />
+                <DetailBox
+                    mode="setting"
+                    title="고객센터"
+                    leftstring={["공지사항", "FAQ", "문의하기"]}
+                />
+            </div>
+            <div className="flex flex-col items-center justify-center mt-7">
+                <CommonButton
+                    size="mini"
+                    isActive={true}
+                    mode="text"
+                    color="black"
+                    detail={{ label: "로그아웃", position: "none" }}
+                />
+                <CommonButton
+                    size="mini"
+                    isActive={true}
+                    mode="text"
+                    color="red"
+                    detail={{ label: "회원 탈퇴", position: "none" }}
+                />
+            </div>
         </div>
-        <div className="flex justify-center gap-3">
-          <div className="w-full">
-            <CouponCount type="available" availablecoup={1} />
-          </div>
-          <div className="w-full">
-            <CouponCount type="used" usedcoup={0} />
-          </div>
-        </div>
-        <div className="flex flex-col mt-7 gap-4">
-          <DetailBox
-            mode="setting"
-            title="내 정보"
-            leftstring={["계정 정보", "비밀번호 변경하기"]}
-          />
-          <DetailBox
-            mode="setting"
-            title="고객센터"
-            leftstring={["공지사항", "FAQ", "문의하기"]}
-          />
-        </div>
-        <div className="flex flex-col items-center justify-center mt-7">
-          <CommonButton
-            size="mini"
-            isActive={true}
-            mode="text"
-            color="black"
-            detail={{ label: "로그아웃", position: "none" }}
-          />
-          <CommonButton
-            size="mini"
-            isActive={true}
-            mode="text"
-            color="red"
-            detail={{ label: "회원 탈퇴", position: "none" }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default UserMypage;
