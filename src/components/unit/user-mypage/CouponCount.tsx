14f0789dd3975dccef1ab39f2ff51918119453fe
aneomagig <imapversion4@gmail.com>
2025-03-30 17:08:17 +0900

    feat: api 연결

--- src/components/unit/user-mypage/CouponCount.tsx
+++ src/components/unit/user-mypage/CouponCount.tsx
@@ -1,56 +1,49 @@
-import React from "react";
 import Icon from "../../common/icons/Icon";
 
-type CouponCountType = "available" | "used";
-
 interface CouponCountProps {
-  type: CouponCountType;
-  availablecoup?: number;
-  usedcoup?: number;
+    type: "available" | "used";
+    count: number;
 }
 
-const CouponCount: React.FC<CouponCountProps> = ({
-  type,
-  availablecoup,
-  usedcoup,
-}) => {
-  const getContent = () => {
-    switch (type) {
-      case "available":
-        return {
-          icon: (
-            <div className="pt-1">
-              <Icon name="couponicon_line" size={35} />
-            </div>
-          ),
-          message: "사용 가능한 쿠폰",
-          coupnum: availablecoup,
-        };
-      case "used":
-        return {
-          icon: (
-            <div className="pt-1.5">
-              <Icon name="usedcouponicon_line" size={30} />
-            </div>
-          ),
-          message: "사용 완료한 쿠폰",
-          coupnum: usedcoup,
-        };
-    }
-  };
-  const { icon, message, coupnum } = getContent();
+const CouponCount: React.FC<CouponCountProps> = ({ type, count }) => {
+    const getContent = () => {
+        switch (type) {
+            case "available":
+                return {
+                    icon: (
+                        <div className="pt-1">
+                            <Icon name="couponicon_line" size={35} />
+                        </div>
+                    ),
+                    message: "사용 가능한 쿠폰",
+                };
+            case "used":
+                return {
+                    icon: (
+                        <div className="pt-1.5">
+                            <Icon name="usedcouponicon_line" size={30} />
+                        </div>
+                    ),
+                    message: "사용 완료한 쿠폰",
+                };
+        }
+    };
 
-  return (
-    <div className="flex flex-col bg-white rounded-xl shadow-custom-basic">
-      <div className="px-4 py-3">
-        <h1 className="text-sm text-black font-semibold pb-2">{message}</h1>
-        <div className="flex items-center gap-4">
-          {icon}
-          <p className="text-2xl font-black text-black">{coupnum}</p>
+    const { icon, message } = getContent();
+
+    return (
+        <div className="flex flex-col bg-white rounded-xl shadow-custom-basic">
+            <div className="px-4 py-3">
+                <h1 className="text-sm text-black font-semibold pb-2">
+                    {message}
+                </h1>
+                <div className="flex items-center gap-4">
+                    {icon}
+                    <p className="text-2xl font-black text-black">{count}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CouponCount;
