5c84fe01a56f140c0a5fade7114cc38e58f2ae8c
aneomagig <imapversion4@gmail.com>
2025-03-30 17:07:04 +0900

    feat: 헤더 아이콘 클릭 시 연결 url 추가

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -1,14 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import Icon from "../common/icons/Icon";
 
 const MainHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="w-full flex justify-between items-center bg-white pt-4">
       <span className="pl-8">
-        <Icon name="logoicon" size={32} />
+        <Icon name="logoicon" size={32} onClick={() => navigate("/usermain")} />
       </span>
       <div className="flex gap-4 pr-8">
-        <Icon name="bellicon_fill" size={28} />
-        <Icon name="personicon_fill" size={25} />
+        <Icon
+          name="bellicon_fill"
+          size={28}
+          className="cursor-pointer"
+          onClick={() => navigate("/usernotification")}
+        />
+        <Icon
+          name="personicon_fill"
+          size={25}
+          className="cursor-pointer"
+          onClick={() => navigate("/usermypage")}
+        />
       </div>
     </header>
   );
