6ab6439bf0d87c05e5892e7c5e9927ca873122f2
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-03 17:25:38 +0900

    feat: 발행목록 조회페이지 추가

--- src/constants/pageTitles.ts
+++ src/constants/pageTitles.ts
@@ -2,6 +2,8 @@ const pageTitles: Record<string, string> = {
     "/join": "회원가입",
     "/partner/coupon/scan": "QR 인식",
     "/user/coupon/register": "쿠폰 등록",
+    "/user/coupon/published": "쿠폰 발행 목록",
+    "/user/coupon/published/deatil": "발행된 쿠폰 통계",
     "/user/main": "사용자 메인화면",
     "/user/notification": "알림함",
     "/user/mypage": "사용자 마이페이지",
@@ -9,4 +11,4 @@ const pageTitles: Record<string, string> = {
     "/user/gift": "선물함",
 };
 
-  export default pageTitles;
+export default pageTitles;
