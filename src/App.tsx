b631dbd406d149e2926169b05cf667de1fb212d7
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-03 17:20:17 +0900

    feat: Route에 발행 쿠폰 조회 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,28 @@ import UserMypage from "./pages/user/UserMypage";
 import UserGift from "./pages/user/UserGift.tsx";
 import QRScanPage from "./pages/partner/QRScanPage.tsx";
 import UserAccountInfo from "./pages/user/UserAccountInfo.tsx";
+import UserCouponPublished from "./pages/user/UserCouponPublished.tsx";
+import UserCouponPublishedDetail from "./pages/user/UserCouponPublishedDetail.tsx";
 
 const App: React.FC = () => {
     return (
         <Routes>
-            <Route path="/user/coupon/register" element={<QRScanPage/>}/>
+            <Route path="/user/coupon/register" element={<QRScanPage />} />
             <Route path="/user/main" element={<UserMain />} />
             <Route path="/user/notification" element={<UserNotification />} />
             <Route path="/user/mypage" element={<UserMypage />} />
             <Route path="/user/gift" element={<UserGift />} />
             <Route path="/user/accountinfo" element={<UserAccountInfo />} />
-            <Route path="/partner/coupon/scan" element={<QRScanPage/>}/>
+            <Route path="/partner/coupon/scan" element={<QRScanPage />} />
             <Route path="/partner/coupon/status/:couponNum" element={<></>} />
+            <Route
+                path="/user/coupon/published"
+                element={<UserCouponPublished />}
+            />
+            <Route
+                path="/user/coupon/published/detail"
+                element={<UserCouponPublishedDetail />}
+            />
         </Routes>
     );
 };
