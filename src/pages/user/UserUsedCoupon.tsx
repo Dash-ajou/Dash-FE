14ffef23090f7086a3582a84dbcb0ca2f5b021cf
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-05 02:13:29 +0900

    feat: 마이페이지에서 couponcount 컴포넌트 onclick 시 navigate 추가

---
+++ src/pages/user/UserUsedCoupon.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout/Layout";
+
+const UserUsedCoupon = () => {
+    return (
+        <Layout>
+            <div></div>
+        </Layout>
+    );
+};
+
+export default UserUsedCoupon;
