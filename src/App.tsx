7b0b3e48bcfff35d6be58d19f17b81d99d71fc3b
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-13 15:45:55 +0900

    feat: 쿠폰 상태 확인 페이지 추가, subheader 수정

--- src/App.tsx
+++ src/App.tsx
@@ -21,35 +21,57 @@ import PartnerOrgDetail from "./pages/partner/PartnerOrgDetail.tsx";
 import PartnerMenuDetail from "./pages/partner/PartnerMenuDetail.tsx";
 import UserCouponPublished from "./pages/user/UserCouponPublished.tsx";
 import UserCouponPublishedDetail from "./pages/user/UserCouponPublishedDetail.tsx";
+import PartnerCoupValCheck from "./pages/partner/PartnerCoupValCheck.tsx";
 
 const App: React.FC = () => {
     return (
         <Routes>
-            <Route path="/" element={<Onboarding/>}/>
-            <Route path="/join" element={<Join/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/findpw" element={<FindPW/>}/>
+            <Route path="/" element={<Onboarding />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/findpw" element={<FindPW />} />
 
-            <Route path="/user/main" element={<UserMain/>}/>
-            <Route path="/user/notification" element={<UserNotification/>}/>
-            <Route path="/user/mypage" element={<UserMypage/>}/>
-            <Route path="/user/mypage/usedcoupon" element={<UserUsedCoupon />} />
-            <Route path="/user/gift" element={<UserGift/>}/>
-            <Route path="/user/accountinfo" element={<UserAccountInfo/>}/>
-            <Route path="/user/coupon/register" element={<QRScanPage/>}/>
-            <Route path="/user/coupon/request" element={<CouponRequest/>}/>
-            <Route path="/user/coupon/request/confirm" element={<CouponRequestConfirm />} />
-            <Route path="/user/coupon/request/list" element={<CouponRequestList/>}/>
-            <Route path="/user/coupon/request/detail" element={<CouponRequestDetail/>}/>
-            <Route path="/user/coupon/published" element={<UserCouponPublished />} />
-            <Route path="/user/coupon/published/detail" element={<UserCouponPublishedDetail />} />
-        
-            <Route path="/partner/coupon/scan" element={<QRScanPage/>}/>
-            <Route path="/partner/coupon/status/:couponNum" element={<></>} />
+            <Route path="/user/main" element={<UserMain />} />
+            <Route path="/user/notification" element={<UserNotification />} />
+            <Route path="/user/mypage" element={<UserMypage />} />
+            <Route
+                path="/user/mypage/usedcoupon"
+                element={<UserUsedCoupon />}
+            />
+            <Route path="/user/gift" element={<UserGift />} />
+            <Route path="/user/accountinfo" element={<UserAccountInfo />} />
+            <Route path="/user/coupon/register" element={<QRScanPage />} />
+            <Route path="/user/coupon/request" element={<CouponRequest />} />
+            <Route
+                path="/user/coupon/request/confirm"
+                element={<CouponRequestConfirm />}
+            />
+            <Route
+                path="/user/coupon/request/list"
+                element={<CouponRequestList />}
+            />
+            <Route
+                path="/user/coupon/request/detail"
+                element={<CouponRequestDetail />}
+            />
+            <Route
+                path="/user/coupon/published"
+                element={<UserCouponPublished />}
+            />
+            <Route
+                path="/user/coupon/published/detail"
+                element={<UserCouponPublishedDetail />}
+            />
+
+            <Route path="/partner/coupon/scan" element={<QRScanPage />} />
+            <Route
+                path="/partner/coupon/status/:couponNum"
+                element={<PartnerCoupValCheck />}
+            />
             <Route path="/partner/main" element={<PartnerMain />} />
             <Route path="/partner/statistics" element={<PartnerStat />} />
             <Route path="/partner/orgdetail" element={<PartnerOrgDetail />} />
-            <Route path="/partner/menudetail" element={<PartnerMenuDetail />} /> 
+            <Route path="/partner/menudetail" element={<PartnerMenuDetail />} />
         </Routes>
     );
 };
