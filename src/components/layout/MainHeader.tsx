ae2da807fc49798401704d5baa392dff6bc12f7e
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-14 12:41:45 +0900

    fix: 아이콘 클릭했을 때 이동되는 페이지 user/partner 분리

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -3,28 +3,40 @@ import Icon from "../common/icons/Icon";
 
 const MainHeader = () => {
     const navigate = useNavigate();
+    const { pathname } = location;
+    const isPartnerPage = pathname.startsWith("/partner");
+
+    const handleLogoClick = () => {
+        navigate(isPartnerPage ? "/partner/main" : "/user/main");
+    };
+
+    const handleNotificationClick = () => {
+        navigate(
+            isPartnerPage ? "/partner/notification" : "/user/notifiaction"
+        );
+    };
+
+    const handleMyPageClick = () => {
+        navigate(isPartnerPage ? "/partner/mypage" : "/user/mypage");
+    };
 
     return (
         <header className="w-full flex justify-between items-center bg-white pt-4">
             <span className="pl-8">
-                <Icon
-                    name="logoicon"
-                    size={32}
-                    onClick={() => navigate("/user/main")}
-                />
+                <Icon name="logoicon" size={32} onClick={handleLogoClick} />
             </span>
             <div className="flex gap-4 pr-8">
                 <Icon
                     name="bellicon_fill"
                     size={28}
                     className="cursor-pointer"
-                    onClick={() => navigate("/user/notification")}
+                    onClick={handleNotificationClick}
                 />
                 <Icon
                     name="personicon_fill"
                     size={25}
                     className="cursor-pointer"
-                    onClick={() => navigate("/user/mypage")}
+                    onClick={handleMyPageClick}
                 />
             </div>
         </header>
