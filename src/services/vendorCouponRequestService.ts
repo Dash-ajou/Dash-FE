ce4d83c8a77d112a52e3aaacaea6518b68362377
PARK-SSOYEON <soyeon00307@gmail.com>
2025-05-22 12:11:02 +0900

    fix: favicon

--- src/services/vendorCouponRequestService.ts
+++ src/services/vendorCouponRequestService.ts
@@ -1,6 +1,6 @@
-import apiClient from "./apiClient.ts";
+import apiClient from "./apiClient.ts"
 
-export const couponRequest =  async() => {
+export const couponRequest = async () => {
     // try {
     //     const response = await apiClient.post('/coupon/issue/request');
     //
@@ -13,22 +13,20 @@ export const couponRequest =  async() => {
     //     return {success: false, error};
     // }
 
-    return {success: true};
+    return { success: true }
 }
 
 export const couponRequestDetail = async (request_id: number) => {
     try {
-        //const response = await apiClient.get(`/coupon/issue/${request_id}`);
-        const response = await apiClient.get('http://127.0.0.1:3658/m1/776425-754479-default/coupon/issue/1');
-        const id = request_id;
+        const response = await apiClient.get(`/coupon/issue/${request_id}`)
 
         if (response.status === 200) {
-            return {success: true, data: response.data.data, id: id};
+            return { success: true, data: response.data.data }
         } else {
-            return {success: false};
+            return { success: false }
         }
     } catch (error) {
-        return {success: false, error};
+        return { success: false, error }
     }
 }
 
@@ -48,56 +46,56 @@ export const couponRequestList = async () => {
     return {
         success: true,
         data: {
-            "page": 1,
-            "size": 10,
-            "count": 3,
-            "data": [
+            page: 1,
+            size: 10,
+            count: 3,
+            data: [
                 {
-                    "request_id": 8675432,
-                    "created_at": "2025-01-05 01:00:00",
-                    "status": "REQUESTED",
-                    "vendor": {
-                        "vendor_name": "아주대학교 총학생회",
-                        "president_name": "나대표",
-                        "president_phone": "01012341243"
+                    request_id: 8675432,
+                    created_at: "2025-01-05 01:00:00",
+                    status: "REQUESTED",
+                    vendor: {
+                        vendor_name: "아주대학교 총학생회",
+                        president_name: "나대표",
+                        president_phone: "01012341243",
                     },
-                    "partner": {
-                        "business_name": "호시 타코야끼",
-                        "owner_name": "나사장",
-                        "owner_phone": "01056785678"
-                    }
+                    partner: {
+                        business_name: "호시 타코야끼",
+                        owner_name: "나사장",
+                        owner_phone: "01056785678",
+                    },
                 },
                 {
-                    "request_id": 8671432,
-                    "created_at": "2025-01-05 01:00:00",
-                    "status": "APPROVED",
-                    "vendor": {
-                        "vendor_name": "아주대학교 사이버보안학과 학생회",
-                        "president_name": "나대표",
-                        "president_phone": "01012341243"
+                    request_id: 8671432,
+                    created_at: "2025-01-05 01:00:00",
+                    status: "APPROVED",
+                    vendor: {
+                        vendor_name: "아주대학교 사이버보안학과 학생회",
+                        president_name: "나대표",
+                        president_phone: "01012341243",
                     },
-                    "partner": {
-                        "business_name": "호시 타코야끼",
-                        "owner_name": "나사장",
-                        "owner_phone": "01056785678"
-                    }
+                    partner: {
+                        business_name: "호시 타코야끼",
+                        owner_name: "나사장",
+                        owner_phone: "01056785678",
+                    },
                 },
                 {
-                    "request_id": 8624432,
-                    "created_at": "2025-01-05 01:00:00",
-                    "status": "DENIED",
-                    "vendor": {
-                        "vendor_name": "아주대학교 총학생회",
-                        "president_name": "나대표",
-                        "president_phone": "01012341243"
+                    request_id: 8624432,
+                    created_at: "2025-01-05 01:00:00",
+                    status: "DENIED",
+                    vendor: {
+                        vendor_name: "아주대학교 총학생회",
+                        president_name: "나대표",
+                        president_phone: "01012341243",
                     },
-                    "partner": {
-                        "business_name": "호시 타코야끼",
-                        "owner_name": "나사장",
-                        "owner_phone": "01056785678"
-                    }
-                }
-            ]
-        }
+                    partner: {
+                        business_name: "호시 타코야끼",
+                        owner_name: "나사장",
+                        owner_phone: "01056785678",
+                    },
+                },
+            ],
+        },
     }
 }
