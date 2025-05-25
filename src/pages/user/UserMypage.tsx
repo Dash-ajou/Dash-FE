1d756cbe6988f507f3a4e78b067bab5878ab9da4
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-25 20:46:38 +0900

    fix: 빌드 오류 수정

--- src/pages/user/UserMypage.tsx
+++ src/pages/user/UserMypage.tsx
@@ -2,92 +2,80 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CommonButton from "../../components/common/button/CommonButton";
 import CouponCount from "../../components/unit/user-mypage/CouponCount";
-import {
-    fetchUserMyPage,
-    UserMyPageData,
-} from "../../services/userMypageService";
+import { fetchUserMyPage, UserMyPageData } from "../../services/userMypageService";
 import DetailBox from "../../components/common/DetailBox";
 import Layout from "../../components/layout/Layout";
 
 const UserMypage = () => {
-    const [userData, setUserData] = useState<UserMyPageData | null>(null);
-    const navigate = useNavigate();
+  const [userData, setUserData] = useState<UserMyPageData | null>(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetchUserMyPage();
-            if (res.success) {
-                setUserData(res.data);
-            }
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetchUserMyPage();
+      if (res.success) {
+        setUserData(res.data);
+      }
+    };
+    fetchData();
+  }, []);
 
-    if (!userData) {
-        return <div className="text-center mt-10">불러오는 중</div>;
-    }
+  if (!userData) {
+    return <div className="text-center mt-10">불러오는 중</div>;
+  }
 
-    return (
-        <Layout>
-            <div className="text-black font-bold text-2xl mt-11 mb-4">
-                <p>{userData.general_name}님 안녕하세요</p>
-            </div>
+  return (
+    <Layout>
+      <div className="text-black font-bold text-2xl mt-11 mb-4">
+        <p>{userData.generalName}님 안녕하세요</p>
+      </div>
 
-            <div className="flex justify-center gap-3">
-                <div className="w-full">
-                    <CouponCount
-                        type="available"
-                        count={userData.coupon_status.usable_coupons}
-                    />
-                </div>
-                <div className="w-full">
-                    <CouponCount
-                        type="used"
-                        count={userData.coupon_status.used_coupons}
-                        onClick={() => navigate("/user/mypage/usedcoupon")}
-                    />
-                </div>
-            </div>
+      <div className="flex justify-center gap-3">
+        <div className="w-full">
+          <CouponCount type="available" count={userData.couponStatus.usableCoupons} />
+        </div>
+        <div className="w-full">
+          <CouponCount
+            type="used"
+            count={userData.couponStatus.usedCoupons}
+            onClick={() => navigate("/user/mypage/usedcoupon")}
+          />
+        </div>
+      </div>
 
-            <div className="flex flex-col mt-7 gap-4">
-                <DetailBox
-                    mode="default"
-                    title="내 정보"
-                    leftstring={userData.menus.my_info.map(
-                        (item) => item.title
-                    )}
-                    linkurl={userData.menus.my_info.map((item) => item.url)}
-                />
-                <DetailBox
-                    mode="default"
-                    title="고객센터"
-                    leftstring={userData.menus.customer_center.map(
-                        (item) => item.title
-                    )}
-                    linkurl={userData.menus.customer_center.map(
-                        (item) => item.url
-                    )}
-                />
-            </div>
+      <div className="flex flex-col mt-7 gap-4">
+        <DetailBox
+          mode="default"
+          title="내 정보"
+          leftstring={userData.menus.myInfo.map((item) => item.title)}
+          linkurl={userData.menus.myInfo.map((item) => item.url)}
+        />
+        <DetailBox
+          mode="default"
+          title="고객센터"
+          leftstring={userData.menus.customerCenter.map((item) => item.title)}
+          linkurl={userData.menus.customerCenter.map((item) => item.url)}
+        />
+      </div>
 
-            <div className="flex flex-col items-center justify-center mt-7">
-                <CommonButton
-                    size="mini"
-                    isActive={true}
-                    mode="text"
-                    color="black"
-                    detail={{ label: "로그아웃", position: "none" }}
-                />
-                <CommonButton
-                    size="mini"
-                    isActive={true}
-                    mode="text"
-                    color="red"
-                    detail={{ label: "회원 탈퇴", position: "none" }}
-                />
-            </div>
-        </Layout>
-    );
+      <div className="flex flex-col items-center justify-center mt-7">
+        <CommonButton
+          size="mini"
+          isActive={true}
+          mode="text"
+          color="black"
+          detail={{ label: "로그아웃", position: "none" }}
+        />
+        <CommonButton
+          size="mini"
+          isActive={true}
+          mode="text"
+          color="red"
+          detail={{ label: "회원 탈퇴", position: "none" }}
+        />
+      </div>
+    </Layout>
+  );
 };
 
 export default UserMypage;
--- src/services/userAccountInfoService.ts
+++ src/services/userAccountInfoService.ts
@@ -1,38 +1,38 @@
 //TO-DO: mock데이터 제거
-import apiClient from './apiClient.ts'
+import apiClient from "./apiClient.ts";
 
 export interface UserAccountInfoData {
-    general_name: string;
-    general_email: string;
-    general_phone: string;
+  general_name: string;
+  general_email: string;
+  general_phone: string;
 }
 
 export interface ApiResponse<T> {
-    status: string;
-    message: string;
-    data: T;
+  status: string;
+  message: string;
+  data: T;
 }
 
 export const fetchUserAccountInfo = async () => {
-    try {
-        // 실제 API 호출 주석 처리
-        const response = await apiClient.get<ApiResponse<UserAccountInfoData>>("/general/account");
-        console.log("user account info 응답:", response.data);
-        if (response.status === 200 && response.data?.data) {
-            return { success: true, data: response.data.data };
-        }
-
-        //TO-DO: 이름은 전역변수에서
-        // const mockResponse: UserAccountInfoData = {
-        //     general_name: "김김김",
-        //     general_email: "등록된 이메일이 없습니다.",
-        //     general_phone: "01012345678",
-        // };
-
-        return { success: true, data: response.data };
-    } catch (error) {
-        console.error("계정 정보 불러오기 실패", error);
+  try {
+    // 실제 API 호출 주석 처리
+    const response = await apiClient.get<ApiResponse<UserAccountInfoData>>("/general/account");
+    console.log("user account info 응답:", response.data);
+    if (response.status === 200 && response.data?.data) {
+      return { success: true, data: response.data.data };
     }
 
-    return { success: false, data: null };
+    //TO-DO: 이름은 전역변수에서
+    // const mockResponse: UserAccountInfoData = {
+    //     general_name: "김김김",
+    //     general_email: "등록된 이메일이 없습니다.",
+    //     general_phone: "01012345678",
+    // };
+
+    return { success: true, data: null };
+  } catch (error) {
+    console.error("계정 정보 불러오기 실패", error);
+  }
+
+  return { success: false, data: null };
 };
