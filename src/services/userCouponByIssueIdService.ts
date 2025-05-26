97382d17a2042eee66bbbde39973e0ddc5434069
PARK-SSOYEON <soyeon00307@gmail.com>
2025-05-26 14:43:33 +0900

    feat: 발행 요청 내역 보기

--- src/services/userCouponByIssueIdService.ts
+++ src/services/userCouponByIssueIdService.ts
@@ -1,37 +1,31 @@
-import apiClient from "./apiClient";
-import { PartnerInfo, ProductInfo } from '../types/CouponRedeemTypes.ts'
+import apiClient from "./apiClient"
+import { PartnerInfo, ProductInfo } from "../types/CouponRedeemTypes.ts"
 
-export type CouponStatus =
-    | "REGISTERABLE"
-    | "USABLE"
-    | "USED"
-    | "EXPIRED"
-    | "DISABLED"
-    | "CANCELLED";
+export type CouponStatus = "REGISTERABLE" | "USABLE" | "USED" | "EXPIRED" | "DISABLED" | "CANCELLED"
 
 export type CouponByIssueID = {
-    id: number;
-    issue_id: number;
-    status: CouponStatus;
-    expired_at: string;
-    product: ProductInfo;
-    partner: PartnerInfo;
-    register_code: string;
-};
+    id: number
+    issue_id: number
+    status: CouponStatus
+    expired_at: string
+    product: ProductInfo
+    partner: PartnerInfo
+    register_code: string
+}
 
 export type CouponByIssueIDResponse = {
-    page: number;
-    size: number;
-    count: number;
-    data: CouponByIssueID[];
-};
+    page: number
+    size: number
+    count: number
+    data: CouponByIssueID[]
+}
 
 export type CouponByIssueIDQuery = {
-    user_name?: string;
-    coupon_status?: CouponStatus;
-    page?: number;
-    size?: number;
-};
+    user_name?: string
+    coupon_status?: CouponStatus
+    page?: number
+    size?: number
+}
 
 export const fetchCouponByIssueID = async (
     issueId: number,
@@ -39,19 +33,17 @@ export const fetchCouponByIssueID = async (
 ): Promise<CouponByIssueIDResponse> => {
     try {
         const response = await apiClient.get<{
-            status: string;
-            message: string;
-            data: CouponByIssueIDResponse;
-        }>(`/coupon/manage/${issueId}/list`, { params });
+            status: string
+            message: string
+            data: CouponByIssueIDResponse
+        }>(`/coupon/manage/${issueId}/list`, { params })
 
-        if (response.data.status !== "SUCCEED") {
-            throw new Error(
-                response.data.message || "쿠폰 상세 정보를 불러오지 못했습니다."
-            );
+        if (response.data.status !== "SUCCESS") {
+            throw new Error(response.data.message || "쿠폰 상세 정보를 불러오지 못했습니다.")
         }
-        return response.data.data;
+        return response.data.data
     } catch (error) {
-        console.error("Failed to fetch coupon by issue ID:", error);
-        return Promise.reject(error);
+        console.error("Failed to fetch coupon by issue ID:", error)
+        return Promise.reject(error)
     }
-};
+}
