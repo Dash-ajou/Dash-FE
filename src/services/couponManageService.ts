0fa495a305cdda01eca756179f004e9e7037598d
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-26 15:14:11 +0900

    fix: user main화면 쿠폰 보이도록 수정

--- src/services/couponManageService.ts
+++ src/services/couponManageService.ts
@@ -6,7 +6,7 @@ export const fetchCouponStatus = async () => {
 
     if (result.status === 200) {
       const {usable_count, used_count} = result.data.data.data;
-      return { success: true, data:{usableCoupons: usable_count, usedCoupons:used_count},};
+      return { success: true, data:{ usableCoupons: usable_count, usedCoupons:used_count },};
     }
   } catch (error) {
     console.error("쿠폰 데이터 불러오기 실패:", error);
