ad52b2004b1c42b16e28d57aab6909af22fac555
PARK-SSOYEON <soyeon00307@gmail.com>
2025-06-09 01:57:16 +0900

    feat: user select-none & 메인 화면 버튼 텍스트 nowrap

--- src/components/unit/user-main/MenuButton.tsx
+++ src/components/unit/user-main/MenuButton.tsx
@@ -1,10 +1,10 @@
-import Icon from "../../common/icons/Icon";
+import Icon from "../../common/icons/Icon"
 
-type MenuButtonType = "newcoup" | "gift" | "couprequest" | "couplist";
+type MenuButtonType = "newcoup" | "gift" | "couprequest" | "couplist"
 
 interface MenuButtonProps {
-    type: MenuButtonType;
-    onClick: () => void;
+    type: MenuButtonType
+    onClick: () => void
 }
 
 const MenuButton: React.FC<MenuButtonProps> = ({ type, onClick }) => {
@@ -14,27 +14,27 @@ const MenuButton: React.FC<MenuButtonProps> = ({ type, onClick }) => {
                 return {
                     icon: <Icon name="newcoupicon" size={50} />,
                     message: "쿠폰 등록",
-                };
+                }
             case "gift":
                 return {
                     icon: <Icon name="gifticon" size={50} />,
                     message: "선물함",
-                };
+                }
             case "couprequest":
                 return {
                     icon: <Icon name="couprequesticon" size={50} />,
                     message: "쿠폰 발행 요청",
-                };
+                }
             case "couplist":
                 return {
                     icon: <Icon name="couplisticon" size={50} />,
                     message: "쿠폰 배부 내역",
-                };
+                }
             default:
-                return { icon: <></>, message: "" };
+                return { icon: <></>, message: "" }
         }
-    };
-    const { icon, message } = getContent();
+    }
+    const { icon, message } = getContent()
 
     return (
         <div className="flex flex-col justify-center items-center">
@@ -42,15 +42,13 @@ const MenuButton: React.FC<MenuButtonProps> = ({ type, onClick }) => {
                 className={`flex rounded-lg shadow-custom-basic flex-col items-center justify-center bg-white menu-button-${type}`}
                 onClick={onClick}
             >
-                <div className="flex items-center justify-center p-2">
-                    {icon}
-                </div>
+                <div className="flex items-center justify-center p-2">{icon}</div>
             </button>
-            <span className="mt-1.5 text-xs font-normal text-black">
+            <span className="mt-1.5 text-xs font-normal text-black whitespace-nowrap">
                 {message}
             </span>
         </div>
-    );
-};
+    )
+}
 
-export default MenuButton;
+export default MenuButton
