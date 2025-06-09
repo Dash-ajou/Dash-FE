e2b5a264ca30da0401bbadee399ae6297a824f04
PARK-SSOYEON <soyeon00307@gmail.com>
2025-06-09 19:18:09 +0900

    fix: code to payment_code

--- src/services/partnerCoupStatusChangeService.ts
+++ src/services/partnerCoupStatusChangeService.ts
@@ -16,24 +16,22 @@ export type CancelCouponResponse = {
     canceled_at: string
 }
 
-export const fetchCouponUse = async (
-    payment_code: string
-): Promise<UseCouponResponse> => {
+export const fetchCouponUse = async (payment_code: string): Promise<UseCouponResponse> => {
     const response = await apiClient.post(
         "/coupon/payment/use",
-        { code: payment_code },
+        { payment_code: payment_code },
         {
             headers: {
                 "Content-Type": "application/json",
             },
         }
-    );
+    )
 
     if (response.data.status !== "SUCCESS") {
-        throw new Error("쿠폰 사용 처리 실패");
+        throw new Error("쿠폰 사용 처리 실패")
     }
-    return response.data.data;
-};
+    return response.data.data
+}
 
 export const fetchCouponCancel = async (
     payment_code: string,
@@ -42,13 +40,11 @@ export const fetchCouponCancel = async (
     const response = await apiClient.post("/coupon/payment/cancel", {
         payment_code,
         redeem_id,
-    });
+    })
 
     if (response.data.status !== "SUCCESS") {
-        throw new Error("쿠폰 사용 취소 실패");
+        throw new Error("쿠폰 사용 취소 실패")
     }
 
-    return response.data.data;
-};
-
-
+    return response.data.data
+}
