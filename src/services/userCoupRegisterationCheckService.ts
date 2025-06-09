8dc1c3239c972b91f96b88ea8a8fbbbbc533b4ee
kyo <90518589+aneomagig@users.noreply.github.com>
2025-06-09 22:53:10 +0900

    fix: 응답 확인 SUCCESS로 변경

--- src/services/userCoupRegisterationCheckService.ts
+++ src/services/userCoupRegisterationCheckService.ts
@@ -33,7 +33,7 @@ export const fetchCouponRegistStatus = async (
     const response = await apiClient.get<ApiResponse>(
         `/coupon/manage/${issueId}/${couponId}`
     );
-    if(response.data.status !== "SUCCEED") {
+    if(response.data.status !== "SUCCESS") {
         throw new Error(
             response.data.message || "쿠폰 등록 상태 정보를 불러오지 못했습니다."
         );
